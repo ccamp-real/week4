f222560b229f7a1323e49381b48a88e08d10c06c
ccamp-real <cmcampise26@gmail.com>
2021-02-11 17:18:59 -0500

    first and ONLY submit boi

---
+++ app1.js
@@ -0,0 +1,31 @@
+console.log("HeLlO WoRLd");
+console.warn("ThIs iS a WaRNinG!");
+console.error("ERoRR! SySTeM CrITiCal!");
+var var1;
+var1 = "ThIS iS a STriNG";
+console.log(var1);
+console.log("\n");
+var var2 = 10;
+var var3 = var1 + var2;
+console.log(var3); //I forgot to guess before I looked, but I thought there would be a space between the string and the number
+/* TyPEs We LeARnEd iN ClASs
+1. String
+2. Boolean
+3. Number
+4. Object
+*/
+console.log(typeof var1); //I totally dont think its a string 
+var var4 = "ThIS iS VaR3";
+console.log(var4);
+var4 = true;
+console.log(typeof var4);
+
+var1 = 5;
+var2 = "5";
+var var2 = parseInt(var2);
+console.log(typeof var2);
+
+var var5 = var1 + var2;
+console.log(var5);
+
+
