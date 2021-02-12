333301b6f95dfbb4a47f262a1e5a8d5a3312d8bd
ccamp-real <cmcampise26@gmail.com>
2021-02-12 17:16:35 -0500

    Arrays and pop-ups and stuff

---
+++ app2.js
@@ -0,0 +1,26 @@
+num1 = prompt("Enter a Number");
+
+num2 = prompt("Enter Another Number");
+
+var num3 = Math.pow(num1,num2);
+
+alert(num3);
+
+var num4 = Math.floor((Math.random() * 3) +1);
+
+console.log(num4);
+
+var alphabet = ["a","b","c","d","e","f",];
+
+alphabet.pop("f");
+
+alphabet.push("g");
+
+alphabet.shift("a");
+
+alphabet.unshift("1");
+
+console.log(alphabet);
+
+
+
